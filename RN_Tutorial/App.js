b976493ea682b9d605f121bda34f2b851894f331
Danbi Kim <beeeg55@gmail.com>
2019-09-13 01:41:32 +0900

    icon

--- RN_Tutorial/App.js
+++ RN_Tutorial/App.js
@@ -1,9 +1,9 @@
 /* eslint-disable react-native/no-inline-styles */
-
 import React from 'react';
 import {Text, View} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 class HomeScreen extends React.Component {
   render() {
@@ -31,9 +31,30 @@ const TabNavigator = createBottomTabNavigator(
     Settings: SettingsScreen,
   },
   {
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({focused, horizontal, tintColor}) => {
+        const {routeName} = navigation.state;
+        let iconName;
+        if (routeName === 'Home') {
+          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+        } else if (routeName === 'Settings') {
+          iconName = `ios-options${focused ? '' : '-outline'}`;
+        }
+
+        // You can return any component that you like here! We usually use an
+        // icon component from react-native-vector-icons
+        return (
+          <Ionicons
+            name={iconName}
+            size={horizontal ? 20 : 25}
+            color={tintColor}
+          />
+        );
+      },
+    }),
     tabBarOptions: {
-      activeTintColor: 'red',
-      inactiveTintColor: 'black',
+      activeTintColor: 'tomato',
+      inactiveTintColor: 'gray',
     },
   },
 );
